55a08651cc50560db38a7ae3f0893c4ce95aab80
markus wolf <markus.wolf@we-are-infinity.com>
2018-12-09 10:50:14 +0100

    Level 9

---
+++ 9/9a.rb
@@ -0,0 +1,47 @@
+input = File.readlines("test_input")[0].split(" ")
+
+players = input[0].to_i
+marbles = input[6].to_i
+
+puts players
+puts marbles
+
+currentGame = Array.new(1, 0)
+playerScore = Hash.new(0)
+currentMarble = 0
+playedMarble = 0
+
+while playedMarble < marbles
+	player = (playedMarble % players) + 1
+	playedMarble += 1
+
+	if playedMarble % 23 == 0
+		removePosition = (currentMarble - 7) % currentGame.size
+		playerScore[player] += playedMarble + currentGame[removePosition]
+		currentGame.delete_at(removePosition)
+		currentMarble = removePosition
+	else
+		if currentGame.size == 1
+			currentMarble = 1
+			currentGame << playedMarble
+		else 
+			nextPosition = ((currentMarble + 1) % currentGame.size + 1)
+			currentMarble = nextPosition
+			currentGame.insert(currentMarble, playedMarble)
+		end
+	end
+
+	# print "Player: #{player} Game: #{currentGame}\n"
+end
+
+maxValue = 0
+maxPlayer = 0
+
+playerScore.each do |k,v|
+	if v > maxValue
+		maxValue = v
+		maxPlayer = k
+	end
+end
+
+print "#{maxPlayer}: #{maxValue}\n"
